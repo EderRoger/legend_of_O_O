10609770ca4ddba30f66f046d39cbf8a5c92868d
Eder Roger <eroger@thoughtworks.com>
2017-10-30 14:10:02 -0200

    add item to CharacterGame

--- src/test/java/com/legend/model/CharacterGameTest.java
+++ src/test/java/com/legend/model/CharacterGameTest.java
@@ -10,9 +10,20 @@ public class CharacterGameTest {
   @Test
   public void testIfCharacterHasAName() {
 
-    CharacterGame characterGame = new CharacterGame();
-    characterGame.setName("Mario");
+    CharacterGame hero = new CharacterGame();
+    hero.setName("Mario");
 
-    assertThat(characterGame.getName(), is("Mario"));
+    assertThat(hero.getName(), is("Mario"));
+
+  }
+
+  @Test
+  public void testIfHasAOneItem(){
+    CharacterGame hero =   new CharacterGame();
+
+    Item item = new Item();
+    hero.addItem(item);
+
+    assertThat(hero.getItems().size(), is(1));
   }
 }
