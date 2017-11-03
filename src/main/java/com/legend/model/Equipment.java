5526b7d7612122ca960d77eab401365004680bcf
Eder Roger <eroger@thoughtworks.com>
2017-11-03 14:53:19 -0200

    add points to equipments

--- src/main/java/com/legend/model/Equipment.java
+++ src/main/java/com/legend/model/Equipment.java
@@ -3,8 +3,18 @@ package com.legend.model;
 public class Equipment {
 
   private String name;
+  private int point;
 
-  public Equipment(String name) {
+  public Equipment(final String name, final int point) {
     this.name = name;
+    this.point = point;
+  }
+
+  public int getPoint() {
+    return point;
+  }
+
+  public void setPoint(int point) {
+    this.point = point;
   }
 }
