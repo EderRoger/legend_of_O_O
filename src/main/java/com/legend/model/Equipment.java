78e8443509e2f0a3bb77b405fc2c9f7d30ced3cd
Eder Roger <eroger@thoughtworks.com>
2017-11-03 15:19:15 -0200

    add type to equipment

--- src/main/java/com/legend/model/Equipment.java
+++ src/main/java/com/legend/model/Equipment.java
@@ -3,18 +3,20 @@ package com.legend.model;
 public class Equipment {
 
   private String name;
+  private String type;
   private int point;
 
-  public Equipment(final String name, final int point) {
+  public Equipment(final String name, final int point, final String type) {
     this.name = name;
     this.point = point;
+    this.type = type;
   }
 
   public int getPoint() {
     return point;
   }
 
-  public void setPoint(int point) {
-    this.point = point;
+  public String getType(){
+    return type;
   }
 }
