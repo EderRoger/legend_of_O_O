978fc6de761215e6f8629da89201bfde4bb3d548
Eder Roger <eroger@thoughtworks.com>
2017-11-09 10:52:58 -0200

    add new test method to check if enemy has a defense and attack point

--- src/test/java/com/legend/model/ScenarioTest.java
+++ src/test/java/com/legend/model/ScenarioTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
 
 public class ScenarioTest {
 
@@ -18,6 +19,27 @@ public class ScenarioTest {
     scenario.addEnemy(enemy);
 
     assertThat(scenario.getEnemies().size(), greaterThan(0));
+
+  }
+
+  @Test
+  public void testIfEnemyHaveAAttackAndDefensePoints(){
+
+    CharacterGame enemy = new CharacterGame();
+    enemy.setName("Bowser");
+    enemy.setType("Enemy");
+
+    Equipment sword = new Equipment("Sword", 10, "Attack");
+    enemy.addEquipment(sword);
+
+    Equipment shield = new Equipment("Shield", 10, "Defense");
+    enemy.addEquipment(shield);
+
+    Scenario scenario = new Scenario();
+    scenario.addEnemy(enemy);
+
+    assertThat(enemy.getAttack(), is(10) );
+    assertThat(enemy.getDefense(), is(10));
 
   }
 }
